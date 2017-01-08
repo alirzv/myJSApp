2aee233dcaceeca21d40079e3fc7ad0f2b1a9b9c
Ali Rizvi 2 <arizv4@sapient.com>
2017-01-08 23:37:06 +0530

    namaz changes

--- www/js/index.js
+++ www/js/index.js
@@ -6,37 +6,18 @@ var wind;
 var direction;
 var iconId;
 var myVar = setInterval(dateAndTime, 1000);
+var date = new Date();
 
 window.onload = function () {
     temp = document.getElementById("temperature");
     humidity = document.getElementById("humidity");
     description = document.getElementById("description");
+	updateByName("newdelhi");
+	displayPrayerTimes();
+	//findFile();
+   }
 
-	updateByZip("newdelhi");
-    /* NEW 
-    if(navigator.geolocation){
-	var showPosition = function(position){
-	    updateByGeo(position.coords.latitude, position.coords.longitude);
-	} 
-	// navigator.geolocation.getCurrentPosition(showPosition);
-	// } else {
-	// var zip = window.prompt("Could not discover your location. What is your zip code?");
-	// updateByZip(zip);
-	// } */
-}
-
-/* NEW
-
-function updateByGeo(lat, lon){
-    var url = "http://api.openweathermap.org/data/2.5/weather?" +
-	"lat=" + lat +
-	"&lon=" + lon +
-	"&APPID=" + APPID;
-    sendRequest(url);    
-} */
-
-
-function updateByZip(zip){
+function updateByName(zip){
     var url = "http://api.openweathermap.org/data/2.5/weather?" +
 	"q=" + zip +
 	"&APPID=" + APPID;
@@ -47,8 +28,8 @@ function updateByZip(zip){
 function sendRequest(url){
     var xmlhttp = new XMLHttpRequest();
    xmlhttp.open("GET", url, true);
-  console.log(url+xmlhttp);
-   console.log(xmlhttp.status == 200);
+   console.log(url+xmlhttp);
+   //console.log(xmlhttp.status == 200);
     xmlhttp.onreadystatechange = function() {
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var data = JSON.parse(xmlhttp.responseText);
@@ -61,7 +42,7 @@ function sendRequest(url){
 	    weather.description = data.weather[0].description;
 	    weather.temp = K2C(data.main.temp);	
 		weather.iconId = data.weather[0].id;
-		console.log(weather.iconId);
+		//console.log(weather.iconId);
 	    update(weather);
 	}else{
 		var weather = {};
@@ -70,7 +51,7 @@ function sendRequest(url){
 		 weather.description = "Data Unavailable. Connection Failure !!"
 		 weather.iconId = "800";
 		 update(weather);
-    console.log("Error");
+		console.log("Showing default weather data");
   }
     };
     xmlhttp.send();
@@ -80,7 +61,6 @@ function update(weather) {
     humidity.innerHTML = weather.humidity;
     temp.innerHTML = weather.temp;
 		description.innerHTML = weather.description;
-  console.log(weather.temp+"temp");
 	showHideIcon(weather.iconId)
 }
 
@@ -102,9 +82,7 @@ function dateAndTime() {
 }
 
 function showHideIcon(iconId){
-	var elements = hide(document.querySelectorAll('.icon'));	
-	//console.log("elementsLength:"+elements.length);
-	console.log("2"+iconId);
+	var elements = hide(document.querySelectorAll('.icon'));
 	showIcon(iconId);
 }
 
@@ -116,7 +94,7 @@ function hide (elements) {
 }
 function showIcon (iconId) {
 	var currentD = new Date();
-	console.log("Hours "+currentD.getHours())
+	//console.log("Hours "+currentD.getHours())
 	if(iconId>=200 && iconId<300){
 		show(document.getElementById('thunder-storm'));
 	}else if(iconId>=300 && iconId<400){
@@ -150,8 +128,59 @@ function showIcon (iconId) {
 function show (elements) {
 	console.log("Showing"+elements);
 	elements.style.display = 'inline-block';
-  // elements = elements.length ? elements : [elements];
-  // for (var index = 0; index < elements.length; index++) {
-    // elements[index].style.display = 'block';
-  // }
 }
+
+function displayPrayerTimes(){
+	var storage = window.localStorage;
+	var handleApiResponse = function(response) {
+		var today = date.toLocaleDateString("en-US",{day: 'numeric'});
+		// Parse JSON string into object
+		//var prayerData = JSON.parse(response);
+		storage.setItem("storedPrayerJSON", response);
+		console.log("Added data to cache:"+response);
+		};
+	var currentMonth = date.toLocaleDateString("en-US",{month: 'short'});
+	if(storage.getItem("storedPrayerJSON") && storage.getItem("storedPrayerJSON").includes(currentMonth)){
+		console.log("Found data in cache"+storage.getItem("storedPrayerJSON"));
+		displayPrayerData(storage.getItem("storedPrayerJSON"));
+		//storage.removeItem(key);
+	}else{
+		callPrayerApi(handleApiResponse);
+		displayPrayerData(storage.getItem("storedPrayerJSON"));
+	}
+}
+
+function displayPrayerData(prayerJsonString){
+	var today = date.toLocaleDateString("en-US",{day: 'numeric'});
+	var dataObj = JSON.parse(prayerJsonString);
+	console.log("Fajr to display"+dataObj.data[today-1].timings.Fajr);
+	document.getElementById("sunrise").innerHTML = dataObj.data[today-1].timings.Sunrise.replace("(IST)",'');
+	document.getElementById("zuhr").innerHTML = dataObj.data[today-1].timings.Dhuhr.replace("(IST)",'');
+	document.getElementById("asr").innerHTML = dataObj.data[today-1].timings.Asr.replace("(IST)",'');
+	document.getElementById("maghrib").innerHTML = dataObj.data[today-1].timings.Maghrib.replace("(IST)",'');
+	document.getElementById("isha").innerHTML = dataObj.data[today-1].timings.Isha.replace("(IST)",'');
+}
+// function findFile(){
+	// var month = date.toLocaleDateString("en-US",{month: 'long'});
+	// var fileName = month+".json";
+	// window.resolveLocalFileSystemURL(cordova.file.applicationDirectory, function (dirEntry) {
+    // console.log('file system open: ' + dirEntry.name);
+	// }, onErrorLoadFs);	
+// }
+
+ function callPrayerApi(callback) { 
+		var month = date.toLocaleDateString("en-US",{month: '2-digit'});
+		var year = date.toLocaleDateString("en-US",{year: 'numeric'});
+		var apiUrl = "http://api.aladhan.com/calendarByCity?city=newDelhi&country=IN&month="+month+"&year="+year+"&method=1&school=1";
+		console.log(apiUrl);
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", apiUrl, true);
+		console.log(xmlhttp.status == 200);
+		xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			   callback(xmlhttp.responseText);
+			   //var weather = {};
+			 }
+		};
+    xmlhttp.send();
+ }
